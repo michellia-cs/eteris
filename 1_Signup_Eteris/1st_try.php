baa7cf010b187af65e8e20ae46830bf0ac604d60
michellia-cs <118815277+michellia-cs@users.noreply.github.com>
2023-05-03 21:07:11 +0700

    Add files via upload

---
+++ 1_Signup_Eteris/1st_try.php
@@ -0,0 +1,10 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    // Get form data
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+
+    // TODO: Add code to insert data into database or perform other actions
+}
+?>
